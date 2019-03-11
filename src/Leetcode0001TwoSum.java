f40e9f07e1dd90c964dcb21b43f022f9826f0418
luxious <liuxinlove1993@gmail.com>
2019-03-11 15:07:58 -0700

    0001
    0001

---
+++ src/Leetcode0001TwoSum.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Leetcode0001TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = target - nums[i];
+            if (map.containsKey(num)) {
+                return new int[] {map.get(num), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[2];
+    }
+}
